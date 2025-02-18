4ec0b96124fe125a28eafe3aef31ba8eb819f127
adriroca20 <hardhe21@gmail.com>
2025-02-18 21:22:25 +0100

    feat: enhance GameInfo component with dynamic turn indication and visual feedback

--- frontend/src/app/tic-tac-toe/components/GameInfo.tsx
+++ frontend/src/app/tic-tac-toe/components/GameInfo.tsx
@@ -1,4 +1,4 @@
-import { Players } from "@/app/enums/Players";
+import { Players } from "@/app/lib/definitions";
 
 interface GameInfoProps {
   playerTurn: Players;
@@ -7,20 +7,26 @@ interface GameInfoProps {
 export const GameInfo = ({ playerTurn }: GameInfoProps) => {
   return (
     <div className="flex flex-col h-full justify-start items-center gap-4 relative">
-        <div className="flex items-center gap-4 mb-4">
-          <div className="flex flex-col items-center">
-            <span className="text-sm font-medium mb-1 text-background">Tú</span>
-            <span className="text-2xl font-bold bg-accent text-black rounded-full w-10 h-10 flex items-center justify-center">
-              X
-            </span>
-          </div>
-          <div className="flex flex-col items-center">
-            <span className="text-sm font-medium mb-1 text-background">Máquina</span>
-            <span className="text-2xl font-bold bg-accent text-black rounded-full w-10 h-10 flex items-center justify-center">
-              O
-            </span>
-          </div>
+      <div className="text-center mb-4">
+        <h2 className="text-xl font-bold text-background mb-2">Partida en Progreso</h2>
+        <p className="text-md text-background/80">
+          Turno de: {playerTurn === Players.X ? "Tú" : "Máquina"}
+        </p>
+      </div>
+      <div className="flex items-center gap-4 mb-4">
+        <div className={`flex flex-col items-center transition-all duration-300 ${playerTurn === Players.X ? 'scale-110' : ''}`}>
+          <span className="text-sm font-medium mb-1 text-background">Tú</span>
+          <span className={`text-2xl font-bold text-black rounded-full w-10 h-10 flex items-center justify-center ${playerTurn === Players.X ? 'bg-accent' : ''}`}>
+            X
+          </span>
         </div>
+        <div className={`flex flex-col items-center transition-all duration-300 ${playerTurn === Players.O ? 'scale-110' : ''}`}>
+          <span className="text-sm font-medium mb-1 text-background">Máquina</span>
+          <span className={`text-2xl font-bold  text-black rounded-full w-10 h-10 flex items-center justify-center ${playerTurn === Players.O ? 'bg-accent' : ''}`}>
+            O
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
